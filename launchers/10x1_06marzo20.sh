e152c7eab65d37607db3b2e4b83a34fe3d86da30
Israel Aguilar <iaguilaror@gmail.com>
2022-08-22 20:59:29 -0500

    add launchers

--- launchers/10x1_06marzo20.sh
+++ launchers/10x1_06marzo20.sh
@@ -7,6 +7,8 @@ nextflow run main.nf \
   --simplecsv "real-data/10x_1_06marzo20/C1_10x_03_06_20.csv" \
   --threads 12 \
   --maxmem 12 \
+	--transcriptome "test/reference/refdata-gex-GRCh38-2020-A" \
+        --chemistry "auto" \
 	-resume \
 	-with-report $output_directory/`date +%Y%m%d_%H%M%S`_report.html \
 	-with-dag $output_directory/`date +%Y%m%d_%H%M%S`.DAG.html
--- launchers/10x_5_14sep20.sh
+++ launchers/10x_5_14sep20.sh
@@ -4,9 +4,11 @@ output_directory="real-data/10x_5_14dic20/nfbcl2counts_10X_results"
 nextflow run main.nf \
 	--input_dir $input_dir \
 	--output_dir $output_directory \
-  --simplecsv "real-data/10x_5_14dic20/C4_10x_5_14dic20.csv" \
-  --threads 12 \
-  --maxmem 12 \
+	--simplecsv "real-data/10x_5_14dic20/C4_10x_5_14dic20.csv" \
+	--threads 12 \
+	--maxmem 12 \
+	--transcriptome "test/reference/refdata-gex-GRCh38-2020-A" \
+	--chemistry "auto" \
 	-resume \
 	-with-report $output_directory/`date +%Y%m%d_%H%M%S`_report.html \
 	-with-dag $output_directory/`date +%Y%m%d_%H%M%S`.DAG.html
