39fccb96ecea1029080944c22a9782f1fa9d1b97
Israel Aguilar <iaguilaror@gmail.com>
2023-01-08 23:44:59 -0600

    clean old dir

--- mkmodules/01-mkfastq/runmk.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf $OUTDIR
-
-## find every vcf file
-#find: -L option to include symlinks
-echo $OUTDIR \
-| xargs mk
--- mkmodules/03b-preliminary-seurat-rawcounts/runmk.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-## find every vcf file
-#find: -L option to include symlinks
-find -L . \
-  -type d \
-  -name "*_cellrangercounts" \
-| sed 's#cellrangercounts#seuratpreliminary_raw.pdf#' \
-| xargs mk
